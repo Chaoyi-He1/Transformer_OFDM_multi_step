4cfeb9c44ce9c55c21a7abe8e36f2ee953d69b8f
Chaoyi-He1 <chaoyi_he@tamu.edu>
2022-07-18 16:15:08 -0500

    Initial commit

--- network.py
+++ network.py
@@ -72,8 +72,8 @@ class Transformer_Decoder_Block(nn.Module):
 
     def decoder_mask(self, inputs):
         num_seq = inputs.size()[1]
-        i = torch.range(0, num_seq - 1, device=config.device)[:, None]
-        j = torch.range(0, num_seq - 1, device=config.devices)
+        i = torch.arange(0, num_seq, device=config.device)[:, None]
+        j = torch.arange(0, num_seq, device=config.device)
         mask = i >= j
         return mask
 
